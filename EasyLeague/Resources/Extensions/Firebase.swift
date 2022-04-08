96c789310e0616b3238b59298a3f58953792abc5
Aly Hirani <hirani.aly@gmail.com>
2022-04-08 15:20:33 -0500

    Refactoring Extensions

---
+++ EasyLeague/Resources/Extensions/Firebase.swift
@@ -0,0 +1,37 @@
+//
+//  Firebase.swift
+//  EasyLeague
+//
+//  Created by Aly Hirani on 4/8/22.
+//
+
+import FirebaseFirestore
+import FirebaseStorage
+
+extension Firestore {
+    
+    var leagueCollection: CollectionReference {
+        collection("leagues")
+    }
+    
+    func leaguesQueryForUser(_ id: String) -> Query {
+        leagueCollection.whereField("memberUserIDs", arrayContains: id)
+    }
+    
+    var userCollection: CollectionReference {
+        collection("users")
+    }
+    
+    func documentForUser(_ id: String) -> DocumentReference {
+        userCollection.document(id)
+    }
+    
+}
+
+extension Storage {
+    
+    func photoReferenceForUser(_ id: String) -> StorageReference {
+        reference().child("photos").child("\(id).png")
+    }
+    
+}
---
+++ EasyLeague/Resources/Extensions/Functionality.swift
@@ -0,0 +1,73 @@
+//
+//  Functionality.swift
+//  EasyLeague
+//
+//  Created by Aly Hirani on 4/8/22.
+//
+
+import UIKit
+
+extension UIViewController: UITextFieldDelegate {
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func add(_ child: UIViewController) {
+        addChild(child)
+        view.addSubview(child.view)
+        child.didMove(toParent: self)
+    }
+    
+    func remove() {
+        guard parent != nil else { return }
+        willMove(toParent: nil)
+        view.removeFromSuperview()
+        removeFromParent()
+    }
+    
+    func presentSimpleAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        present(alert, animated: true)
+    }
+    
+    func addSpinner() -> SpinnerViewController {
+        let spinner = SpinnerViewController()
+        add(spinner)
+        return spinner
+    }
+    
+    func withAutoLayout<V: UIView>(_ subview: V) -> V {
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        return subview
+    }
+    
+    func popFromNavigation() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func constrainToSafeArea(_ subview: UIView) {
+        NSLayoutConstraint.activate([
+            subview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            subview.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            subview.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            subview.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+        ])
+    }
+    
+}
+
+extension Int {
+    
+    init?(_ description: String?) {
+        guard let description = description else { return nil }
+        self.init(description)
+    }
+    
+}
