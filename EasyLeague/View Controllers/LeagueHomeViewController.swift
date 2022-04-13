915e51ebabea4d339873b7bfa4f6ba5ac1ddcfc0
Daniel Deng <ddeng154@gmail.com>
2022-04-12 19:03:13 -0500

    Made table for league view

--- EasyLeague/View Controllers/LeagueHomeViewController.swift
+++ EasyLeague/View Controllers/LeagueHomeViewController.swift
@@ -17,6 +17,16 @@ class LeagueHomeViewController: UIViewController {
     
     var team: Team!
     
+    let contentCellIdentifier = "ContentCellIdentifier"
+    
+    lazy var teamsCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: CustomCollectionViewLayout())
+        collectionView.register(ContentCollectionViewCell.self, forCellWithReuseIdentifier: contentCellIdentifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        return withAutoLayout(collectionView)
+    }()
+    
     lazy var inviteButton = createBarButton(item: .action, action: #selector(inviteButtonPressed))
     
     lazy var spacer = createSpacer()
@@ -40,6 +50,7 @@ class LeagueHomeViewController: UIViewController {
         navigationItem.title = team.name
         navigationItem.rightBarButtonItem = inviteButton
         
+        stackView.addArrangedSubview(teamsCollectionView)
         stackView.addArrangedSubview(leagueStartedLabel)
         stackView.addArrangedSubview(spacer)
         if league.isStarted {
@@ -93,5 +104,50 @@ class LeagueHomeViewController: UIViewController {
         leagueInfoController.league = league
         show(leagueInfoController, sender: self)
     }
+    
+}
+
+
+extension LeagueHomeViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return league.teams.count + 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return league.teamStats.count + 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: contentCellIdentifier, for: indexPath) as! ContentCollectionViewCell
+        if indexPath.section % 2 != 0 {
+                    cell.backgroundColor = UIColor(white: 242/255.0, alpha: 1.0)
+                } else {
+                    cell.backgroundColor = UIColor.white
+                }
+
+                if indexPath.section == 0 {
+                    if indexPath.row == 0 {
+                        cell.label.text = "Teams"
+                        cell.label.font = UIFont.boldSystemFont(ofSize: cell.label.font.pointSize)
+                    } else {
+                        var sortedKeys = Array(league.teamStats.keys)
+                        sortedKeys.sort(by: {$0 < $1})
+                        cell.label.text = sortedKeys[indexPath.row - 1]
+                        cell.label.font = UIFont.boldSystemFont(ofSize: cell.label.font.pointSize)
+                    }
+                } else {
+                    if indexPath.row == 0 {
+                        cell.label.text = league.teams[indexPath.section - 1].name
+                        cell.label.font = UIFont.boldSystemFont(ofSize: cell.label.font.pointSize)
+                    } else {
+                        cell.label.text = "Content"
+                    }
+                }
+
+                return cell
+    }
+}
+
+extension LeagueHomeViewController: UICollectionViewDelegate {
     
 }
